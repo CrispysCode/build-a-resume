5f372efcaa0ec010adba239ccfbdfe39b0fb79af
CrispysCode <pr0ject0dinx@gmail.com>
2025-05-23 19:12:08 -0500

    added handlechange event handler to general component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import "../styles/GeneralInfo.css";
 
-
-export default function GeneralInfo() {
+export default function GeneralInfo({ data, onChange }) {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    onChange({ ...data, [name]: value });
+  };
   return (
     <div className="general">
       <h2>General Information</h2>
       <label>
         Name:
-        <input type="text"></input>
+        <input
+          type="text"
+          name="name"
+          value={data.name}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Phone:
-        <input type="tel"></input>
+        <input
+          type="tel"
+          name="phone"
+          value={data.phone}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Email:
-        <input type="email"></input>
+        <input
+          type="email"
+          name="email"
+          value={data.email}
+          onChange={handleChange}
+        />
       </label>
     </div>
   );
