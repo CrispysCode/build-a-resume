ef3c4034ad1f7a8076391c8d0bc0c569685aa927
CrispysCode <pr0ject0dinx@gmail.com>
2025-05-29 16:44:53 -0500

    Added logic to render the resume as the user types in their information

--- src/components/JobInfo.jsx
+++ src/components/JobInfo.jsx
@@ -1,30 +1,59 @@
 import React from "react";
-import "../styles/JobInfo.css"
+import "../styles/JobInfo.css";
 
+export default function JobInfo({ data, onChange }) {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    onChange({ ...data, [name]: value });
+  };
 
-export default function JobInfo() {
   return (
     <div className="job">
       <h2>Job History</h2>
       <label>
         Company
-        <input type="text"></input>
+        <input
+          type="text"
+          name="company"
+          value={data.company}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Position
-        <input type="text"></input>
+        <input
+          type="text"
+          name="position"
+          value={data.position}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Duties
-        <input type="text"></input>
+        <input
+          type="text"
+          name="duties"
+          value={data.duties}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Start Date:
-        <input type="date"></input>
+        <input
+          type="date"
+          name="startDate"
+          value={data.startDate}
+          onChange={handleChange}
+        />
       </label>
       <label>
         End Date:
-        <input type="date"></input>
+        <input
+          type="date"
+          name="endDate"
+          value={data.endDate}
+          onChange={handleChange}
+        />
       </label>
     </div>
   );
