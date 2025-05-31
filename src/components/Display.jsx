94b92a8b0fa6d710b5d4d8ec117f218baf9e697d
CrispysCode <pr0ject0dinx@gmail.com>
2025-05-31 00:16:30 -0500

    user can now add multiple employment history components

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,31 +1,34 @@
 import React from "react";
-import "../styles/Display.css"
+import "../styles/Display.css";
 export default function Display({ resume }) {
-   const { general, education, work } = resume;
-     return (
-        <div className="display">
-            
-            <h2>General Information</h2>
-            <p>{general.name}</p>
-            <p>{general.phone}</p>
-            <p>{general.email}</p>
+  const { general, education, work } = resume;
+  return (
+    <div className="display">
+      <h2>General Information</h2>
+      <p>{general.name}</p>
+      <p>{general.phone}</p>
+      <p>{general.email}</p>
 
-            <h2>Education Information</h2>
-            {education.map((edu, index) => (
-               <div key={index} className="education-entry">
-               <p>{edu.school}</p>
-            <p>{edu.study}</p>
-            <p>{edu.gradDate}</p>
-            </div>      
-            ))}
-            
-            <h2>Employment History</h2>
-            <p>{work.company}</p>
-            <p>{work.position}</p>
-            <p>{work.duties}</p>
-            <p>{work.startDate}  {work.endDate} </p>
-            
+      <h2>Education Information</h2>
+      {education.map((edu, index) => (
+        <div key={index} className="education-entry">
+          <p>{edu.school}</p>
+          <p>{edu.study}</p>
+          <p>{edu.gradDate}</p>
         </div>
-     )
+      ))}
 
+      <h2>Employment History</h2>
+      {work.map((job, index) => (
+        <div key={index} className="job-entry">
+          <p>{job.company}</p>
+          <p>{job.position}</p>
+          <p>{job.duties}</p>
+          <p>
+            {job.from} - {job.to}{" "}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/components/JobInfo.jsx
+++ src/components/JobInfo.jsx
@@ -5,68 +5,66 @@ export default function JobInfo({ data, onChange, addJob }) {
   const handleChange = (index, e) => {
     const { name, value } = e.target;
     const updatedJobs = data.map((job, i) =>
-    i === index ? {...job, [name]: value } : job
-  );
+      i === index ? { ...job, [name]: value } : job
+    );
     onChange(updatedJobs);
   };
 
   return (
     <div className="job">
       <h2>Job History</h2>
-    {data.map((job, index) => (
-      <div key={index} className="jobEntry">
-        
-
-      <label>
-        Company
-        <input
-          type="text"
-          name="company"
-          value={job.company}
-          onChange={(e) => handleChange(index, e)}
-        />
-      </label>
-      <label>
-        Position
-        <input
-          type="text"
-          name="position"
-          value={job.position}
-          onChange={(e) => handleChange(index, e)}
-        />
-      </label>
-      <label>
-        Duties
-        <input
-          type="text"
-          name="duties"
-          value={job.duties}
-          onChange={(e) => handleChange(index, e)}
-        />
-      </label>
-      <label>
-        Start Date:
-        <input
-          type="date"
-          name="from"
-          value={job.from}
-          onChange={(e) => handleChange(index, e)}
-        />
-      </label>
-      <label>
-        End Date:
-        <input
-          type="date"
-          name="to"
-          value={job.to}
-          onChange={(e) => handleChange(index, e)}
-        />
-      </label>
-    </div>
-    ))}
-    <button type="button" onClick={addJob} className="addJob">
-      Add Experience
-    </button>
+      {data.map((job, index) => (
+        <div key={index} className="jobEntry">
+          <label>
+            Company
+            <input
+              type="text"
+              name="company"
+              value={job.company}
+              onChange={(e) => handleChange(index, e)}
+            />
+          </label>
+          <label>
+            Position
+            <input
+              type="text"
+              name="position"
+              value={job.position}
+              onChange={(e) => handleChange(index, e)}
+            />
+          </label>
+          <label>
+            Duties
+            <input
+              type="text"
+              name="duties"
+              value={job.duties}
+              onChange={(e) => handleChange(index, e)}
+            />
+          </label>
+          <label>
+            Start Date:
+            <input
+              type="date"
+              name="from"
+              value={job.from}
+              onChange={(e) => handleChange(index, e)}
+            />
+          </label>
+          <label>
+            End Date:
+            <input
+              type="date"
+              name="to"
+              value={job.to}
+              onChange={(e) => handleChange(index, e)}
+            />
+          </label>
+        </div>
+      ))}
+      <button type="button" onClick={addJob} className="addJob">
+        Add Experience
+      </button>
     </div>
   );
 }
